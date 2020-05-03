b975c50845beaa25cdfb9e399af033e9b0699272
Cam <j.c.doan@outlook.com>
2020-05-03 16:53:19 -0600

    Fixed product module imports.

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PromoBoxComponent } from '@app/modules/product/components/promo-box/promo-box.component';
+import { ActionButtonComponent } from '@product/components/action-button/action-button.component';
+import { BadgesComponent } from '@product/components/badge/badges.component';
+import { OfferingComponent } from '@product/components/offering/offering.component';
+import { RatingComponent } from '@product/components/rating/rating.component';
 import { ProductPage } from '@product/pages/product/product.page';
 import { SharedModule } from '@shared/shared.module';
-import { ActionButtonComponent } from './components/action-button/action-button.component';
-import { BadgesComponent } from './components/badge/badges.component';
-import { OfferingComponent } from './components/offering/offering.component';
-import { RatingComponent } from './components/rating/rating.component';
 
 export const productRoutes: Routes = [
     {
